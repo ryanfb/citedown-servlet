d1a601148e0c11a14ad6dcea14e55a294ea61245
Ryan Baumann <ryan.baumann@gmail.com>
2013-05-19 17:15:16 -0400

    Rename and remove unneeded

--- src/main/java/info/papyri/xsugar/splitter/SplitterJoiner.java
+++
@@ -1,8 +0,0 @@
-package info.papyri.xsugar.splitter;
-
-import java.util.List;
-
-public interface SplitterJoiner {
-  public List<String> split(String in) throws Exception;
-  public String join(List<String> in) throws Exception;
-}
